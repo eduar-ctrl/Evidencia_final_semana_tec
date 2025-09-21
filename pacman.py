fbfe3d226ba90009f81520e089b6050a64bdb81e
Liam <liamfelixb@outlook.com>
2025-09-20 20:39:01 -0600

    ajustar velocidad de fantasmas de 5 a 8 pixeles por paso

--- pacman.py
+++ pacman.py
@@ -25,10 +25,10 @@ writer = t.Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(8, 0)],
+    [vector(-180, -160), vector(0, 8)],
+    [vector(100, 160), vector(0, -8)],
+    [vector(100, -160), vector(-8, 0)],
 ]
 # fmt: off
 tiles = [
